3314f47ef823a2c378acf5ff2c91f366bc97e0cf
beniepedia <beniepedia@gmail.com>
2019-07-05 15:00:38 +0700

    menamabah tombol share

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -3,28 +3,52 @@
 		<div class="row">
 			<div class="col">
 				<ul>
-					<li><a href="https://www.facebook.com/profile.php?id=100010250193567"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-				</li>
-				<li><a href="https://www.instagram.com/id_mjparfum_official/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-			</li>
-			<li><a href="https://api.whatsapp.com/send?phone=6287772360300&text=Hi%20*admin*,%20saya%20mau%20pesen%20parfum%20nya."
+					<li><a href="https://www.facebook.com/profile.php?id=100010250193567"><i 	class="fa fa-facebook" aria-hidden="true"></i></a>
+					</li>
+					<li><a href="https://www.instagram.com/id_mjparfum_official/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					</li>
+					<li><a href="https://api.whatsapp.com/send?phone=6287772360300&text=Hi%20*admin*,%20saya%20mau%20pesen%20parfum%20nya."
 			target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
-		</li>
-	</ul>
-</div>
-</div>
-</div>
-</section>
-<section id="footer">
-<footer class="bg-dark text-white">
-<div class="container pt-3">
-<div class="row text-center">
-	<div class="col">
-		<p>&copy; Copyright <?= date("Y"); ?>. Create by <a href="https://www.facebook.com/ahmadqomaini">Ahmad Qomaini</a></p>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
-</div>
-</div>
-</footer>
+</section>
+
+<!-- Button Share -->
+<section id="btn-share">
+	<footer class="bg-secondary text-white ">
+		<div class="container pt-5" style="margin-bottom: -80px">
+			<h3 class="text-center">Bagikan :</h3>
+			<div class="row text-center pt-5 pb-3 justify-content-center">
+				<!-- AddToAny BEGIN -->
+				<div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+				<a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+				<a class="a2a_button_facebook"></a>
+				<a class="a2a_button_twitter"></a>
+				<a class="a2a_button_email"></a>
+				<a class="a2a_button_whatsapp"></a>
+				<a class="a2a_button_telegram"></a>
+				<a class="a2a_button_google_gmail"></a>
+				</div>
+				<!-- AddToAny END -->
+			</div>
+		</div>
+	</footer>
+</section>
+
+<!-- Footer -->
+<section id="footer">
+	<footer class="bg-dark text-white">
+		<div class="container pt-3">
+			<div class="row text-center">
+				<div class="col">
+					<p>&copy; Copyright <?= date("Y"); ?>. Create by <a href="https://www.facebook.com/ahmadqomaini">Ahmad Qomaini</a></p>
+				</div>
+			</div>
+		</div>
+	</footer>
 </section>
 
 <!-- Optional JavaScript -->
@@ -42,6 +66,11 @@ crossorigin="anonymous"></script>
 <script id="intergram" type="text/javascript" src="https://www.intergram.xyz/js/widget.js"></script>
 <script  type="text/javascript" src="<?= base_url('assets/') ?>js/chat.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+<script>
+	var a2a_config = a2a_config || {};
+	a2a_config.locale = "id";
+</script>
+<script async src="https://static.addtoany.com/menu/page.js"></script>
 <script>
 	$(document).ready(function(){
 		var wow = new WOW({
