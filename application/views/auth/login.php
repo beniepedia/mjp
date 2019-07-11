82e2eb0b4a3b2895ac8718bf1ef52f37c72b45fa
beniepedia <beniepedia@gmail.com>
2019-07-11 10:44:16 +0700

    mengahapus ion auth

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,28 +1,56 @@
-<h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
+    <!-- Outer Row -->
+    <div class="row justify-content-center mt-5">
 
-<div id="infoMessage"><?php echo $message;?></div>
+      <div class="col-xl-10 col-lg-12 col-md-9">
 
-<?php echo form_open("auth/login");?>
+        <div class="card o-hidden border-0 shadow-lg my-5 ">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Silahkan Login!</h1>
+                  </div>
+                  <form class="user" action="<?= base_url('auth/login'); ?>" method="post">
+                    <div class="form-group">
+                      <input type="email" class="form-control form-control-user"placeholder="Enter Email Address..." name="email">
+                      ><?= form_error('email'); ?>
+                    </div>
+                    
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" placeholder="Password" name="password">
+                    </div>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input type="checkbox" class="custom-control-input" id="customCheck">
+                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                      </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                      Login
+                    </button>
+                    <hr>
+                   <!--  <a href="index.html" class="btn btn-google btn-user btn-block">
+                      <i class="fab fa-google fa-fw"></i> Login with Google
+                    </a>
+                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                    </a> -->
+                  </form>
+                  <div class="text-center">
+                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                  </div>
+                  <div class="text-center">
+                    <a class="small" href="register.html">Create an Account!</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-  <p>
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
-  </p>
+      </div>
 
-  <p>
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_remember_label', 'remember');?>
-    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
-
-
-  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
-
-<?php echo form_close();?>
-
-<p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+    </div>
