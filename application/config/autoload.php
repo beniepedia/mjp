ce2f5b24373930df48f05ebb225e89ede50473fd
beniepedia <beniepedia@gmail.com>
2019-07-16 01:29:29 +0700

    megaktifkan helper security

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'flashm', 'sendemail','benie');
+$autoload['helper'] = array('url', 'flashm', 'sendemail','benie','security');
 
 /*
 | -------------------------------------------------------------------
